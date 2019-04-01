68ef1060c99df8f237254e5ae662747c369e4661
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2019-04-01 23:01:15 +0300

    identity

---
+++ Source/ReWork.DataProvider/Identity/AppRoleManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ReWork.DataProvider.Entities;
+
+namespace ReWork.DataProvider.Identity
+{
+    public class AppRoleManager : RoleManager<Role>
+    {
+        public AppRoleManager(RoleStore<Role> store)
+            : base(store)
+        {
+
+        }
+    }
+}
---
+++ Source/ReWork.DataProvider/Identity/AppUserManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity;
+using ReWork.DataProvider.Entities;
+
+namespace ReWork.DataProvider.Identity
+{
+    public class AppUserManager : UserManager<User>    
+    {
+        public AppUserManager(IUserStore<User> store)
+            :base(store)
+        {
+
+        }
+    }
+}
