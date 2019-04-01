ef615c02a529e7a8188d07fc9311b105b3d6a409
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2019-04-01 23:02:08 +0300

    Add OperationDetails

---
+++ Source/ReWork.Logic/Infustructure/OperationDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace ReWork.Logic.Infustructure
+{
+    public class OperationDetails
+    {
+        public OperationDetails(bool succedeed, string propertyName, string message)
+        {
+            Succedeed = succedeed;
+            PropertyName = propertyName;
+            Message = message;
+        }
+
+        public bool Succedeed { get; set; }
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+    }
+}
