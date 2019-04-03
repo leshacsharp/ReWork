56da4369797de4e29abbaf5cf336bdfe0be2a3c0
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2019-04-03 23:00:59 +0300

    create ioc

---
+++ Source/ReWork.WebSite/App_Start/IocConfig.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using ReWork.Logic.Ioc;
+using ReWork.WebSite.Ioc;
+using System.Web.Mvc;
+
+namespace ReWork.WebSite.App_Start
+{
+    public class IocConfig
+    {
+        public static void ConfigureContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            builder.RegisterModule<DbModule>();
+            builder.RegisterModule<AuthorizeModule>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ Source/ReWork.WebSite/App_Start/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(ReWork.WebSite.App_Start.Startup))]
+namespace ReWork.WebSite.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/account/login"),
+            });
+        }
+    }
+}
